f469914e57abb8c017bb6c4b3a1cacbc82ca2d6f
thientm <thien.tm@citigo.com.vn>
2022-07-26 17:08:13 +0700

    change to telebot

--- controller.ts
+++ controller.ts
@@ -19,10 +19,30 @@ const receivedWebhookR2Gitlab = async ({
         return;
     }
 
-    const content = await request.body().value;
-    console.log(content);
-    response.body = { message: "Received msg: " + content };
+    const webhookContent = await request.body().value;    
+    //response.body = { message: "Received msg: " + content };
+
+    let pipelinesInfoText = getPipelinesInfo(webhookContent);
+    
+    await sentToTelegramApi(pipelinesInfoText);
+
     response.status = 200;
 };
+
+let sentToTelegramApi = async function(text) {
+    //configTelegramBot.url = 'https://api.telegram.org/bot5337368328:AAH7FQeGeXk6AvubjpvuMYuqclTPI9mwYhs/sendMessage?chat_id=-1001531122216&text=' + encodeURI(text) 
+    const teleBotEnpoint = 'https://api.telegram.org/bot5337368328:AAH7FQeGeXk6AvubjpvuMYuqclTPI9mwYhs/sendMessage?chat_id=-717245078&text=' + encodeURI(text) 
+    
+    let resp = await fetch(teleBotEnpoint);
+    console.log(resp.status); // 200
+}
+
+let getPipelinesInfo = function(pipeData) {
+    let pipeObj = '🚀 ' + (pipeData.object_attributes.tag ? 'New Tag: ': '') + pipeData.object_attributes.ref + '\n'
+                    + '👷‍♂️ ' + pipeData.user.username + ': ' + pipeData.commit.title + '\n'
+                    + '⚡ Status: ' + pipeData.object_attributes.status + ' -> '+ pipeData.object_attributes.detailed_status;    
+    return pipeObj;
+}
+
 
 export { getR2gitlab, receivedWebhookR2Gitlab };
