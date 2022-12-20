3b7961b5627a7762b133c7abfd34b97c9a6b35f1
thientm <thientm1992@gmail.com>
2022-12-21 00:42:53 +0700

    update ui

--- controller.ts
+++ controller.ts
@@ -2,12 +2,7 @@ const providerCfg = Deno.env.get("providerCfg");
 
 const getR2gitlab = ({ response }: { response: any }) => {
   response.body =
-    "GET OK - Api worked! - webhook: POST /r2gitlab,  test sent to workplace: GET /testSentToWorkplaceChat";
-};
-
-const testSentToWorkplaceChat = async ({ response }: { response: any }) => {
-  await sentToWorkplaceChat("Api worked!");
-  response.body = "GET OK - Api worked! Check Wp for sent!";
+    "GET OK - Api worked! - webhook: POST /r2gitlab";
 };
 
 const receivedWebhookR2Gitlab = async ({
@@ -33,7 +28,7 @@ const receivedWebhookR2Gitlab = async ({
   if (providerCfg && providerCfg === "1") {
     await sentToTelegramApi(pipelinesInfoText);
   } else if (providerCfg && providerCfg === "2") {
-    sentToWorkplaceChat(pipelinesInfoText);
+    
   } else if (providerCfg && providerCfg === "3") {
     await sentToTelegramApiDev(pipelinesInfoText);
   } else {
@@ -60,76 +55,6 @@ const sentToTelegramApi = async function (text: string) {
   console.log(resp.status + ": Tele Sent!"); // 200
 };
 
-const sentToWorkplaceChat = function (msgtoSent: string) {
-  //Header builder
-  const myHeaders = new Headers();
-  myHeaders.append("authority", "citigo.m.workplace.com");
-  myHeaders.append("accept", "*/*");
-  myHeaders.append("accept-language", "en-US,en;q=0.9,vi;q=0.8,la;q=0.7");
-  myHeaders.append("content-type", "application/x-www-form-urlencoded");
-  myHeaders.append("dnt", "1");
-  myHeaders.append("origin", "https://citigo.m.workplace.com");
-  myHeaders.append(
-    "referer",
-    "https://citigo.m.workplace.com/chat/t/5401287386619630",
-  );
-  myHeaders.append("sec-fetch-dest", "empty");
-  myHeaders.append("sec-fetch-mode", "cors");
-  myHeaders.append("sec-fetch-site", "same-origin");
-  myHeaders.append(
-    "user-agent",
-    "Mozilla/5.0 (Linux; Android 9.0; SAMSUNG SM-F900U Build/PPR1.180610.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
-  );  
-  myHeaders.append("x-msgr-region", "PRN");
-  myHeaders.append("x-requested-with", "XMLHttpRequest");
-  myHeaders.append("x-response-format", "JSONStream");
-  // Cookie may change
-  myHeaders.append("x-fb-lsd", "XvRFYOVqnZcLs1C9VZA3g6");
-  myHeaders.append(
-    "cookie",
-    "datr=4he9Ykshdo-z0VylQtFho8KW; sb=EBi9YrIuKz_fTS10xUZdZfCv; c_user=100042843791126; presence=EDvF3EtimeF1657253794EuserFA21B42843791126A2EstateFDutF0CEchF_7bCC; dpr=3; wd=390x844; m_pixel_ratio=3; xs=8%3ALenKhBqXwvz8Xw%3A2%3A1656559633%3A-1%3A-1%3A%3AAcUDxYbze0973oSh7vENAzL49lTUm3U3XgMb2C4VZPDs",
-  );
-  // END Header builder
-  // Body builder
-  const bodyEncoded = new URLSearchParams();
-  bodyEncoded.append("tids", "cid.g.5401287386619630");
-  bodyEncoded.append("wwwupp", "C3");
-  bodyEncoded.append("waterfall_source", "message");
-  bodyEncoded.append("action_time", "1668155442141");
-  bodyEncoded.append("__cid", "750043595418020");
-  bodyEncoded.append("lsd", "XvRFYOVqnZcLs1C9VZA3g6");
-  bodyEncoded.append("__user", "100042843791126");
-  // may change
-  bodyEncoded.append(
-    "fb_dtsg",
-    "NAcP2djQGCFnjSOYPFagE5RXFA6MGEWwowFYC33R9gql26clR_ZmLJg:37:1658895813",
-  );
-  bodyEncoded.append("jazoest", "25305");
-  bodyEncoded.append(
-    "__dyn",
-    "1KQEGiEiwgVU-4UpwGzVQ2mml3onxG6U4a6EC5UfQE6C2W3q327E2JxK4o19oe8hwem0Joeoe852q1ew65xO0FE6S1QzU1vrzo1sE52229wcq0C9EdE2IzUuw9O1Awci1qw8W1uwa-7U881soow46wbS1Lwqo2Yw",
-  );
-  bodyEncoded.append("__req", "b");
-  bodyEncoded.append(
-    "__a",
-    "AYkV8secHl78WKdH02O1Ay6rlSqEbInqQ7uVMu3HUlrzZXB6_xFhfMwwmcOAE84gZLk_O8J0I9f6dWTjppH0uM5E2knRjZ2Aj0lT9MO-HCnEYA",
-  );
-
-  bodyEncoded.append("body", msgtoSent);
-  // END Body builder
-
-  console.log("Workplace-Chat Requested!");
-  const body = bodyEncoded;
-  fetch(
-    "https://citigo.m.workplace.com/messages/send/?icm=1",
-    {
-      method: "POST",
-      headers: myHeaders,
-      body,
-    },
-  );
-};
-
 const parseTagEnv = (tagName: string) => {
   if (tagName.indexOf("dc2s-retail") > -1) {
     return "Staging";
@@ -143,18 +68,21 @@ const parseTagEnv = (tagName: string) => {
 };
 
 const tagInfoBuilder = (pipeData) => {
-  let pipeObj = "🚀 Tag " + parseTagEnv(pipeData.object_attributes.ref) + ": " +
+  let pipeObj = "🚀 Tag" + ": " +
     pipeData.object_attributes.ref + "\n" +
-    "👷‍♂️ " + pipeData.user.username + ": " + "⚡ " +
+    "👷‍♂️ " + pipeData.user.username + ": " + parseTagEnv(pipeData.object_attributes.ref) + "\n"
+  statusPipe === 'success' ? '✔' : '❌' +
     pipeData.object_attributes.status + " -> " +
     pipeData.object_attributes.detailed_status;
   return pipeObj;
 };
 
 let getPipelinesInfo = function (pipeData) {
+  const statusPipe = pipeData.object_attributes.status;
   if (
-    pipeData.object_attributes.status === "pending" ||
-    pipeData.object_attributes.status === "canceled"
+    statusPipe === "pending" ||
+    statusPipe === "running" ||
+    statusPipe === "canceled"
   ) {
     return "";
   }
@@ -164,10 +92,11 @@ let getPipelinesInfo = function (pipeData) {
   } else {
     let pipeObj = "👀 " + pipeData.object_attributes.ref + "\n" +
       "👷‍♂️ " + pipeData.user.username + ": " + pipeData.commit.title + "\n" +
-      "⚡ Status: " + pipeData.object_attributes.status + " -> " +
-      pipeData.object_attributes.detailed_status;
+      statusPipe === 'success' ? '✔' : '❌' +
+      " Status: " + pipeData.object_attributes.status + " -> " +
+    pipeData.object_attributes.detailed_status;
     return pipeObj;
   }
 };
 
-export { getR2gitlab, receivedWebhookR2Gitlab, testSentToWorkplaceChat };
+export { getR2gitlab, receivedWebhookR2Gitlab };
--- routes.ts
+++ routes.ts
@@ -1,11 +1,9 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
 import {
   getR2gitlab,
-  receivedWebhookR2Gitlab,
-  testSentToWorkplaceChat,
+  receivedWebhookR2Gitlab
 } from "./controller.ts";
 
 const router = new Router();
 router.get("/", getR2gitlab).post("/r2gitlab", receivedWebhookR2Gitlab);
-router.get("/testSentToWorkplaceChat", testSentToWorkplaceChat);
 export default router;
